d0bb6b3e48b80c82ef9f0e2824c054ec8e6d37c6
Joera <joeramulders@gmail.com>
2023-01-23 13:36:54 +0100

    opgeschoond

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
@@ -50,10 +47,7 @@ const config = {
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset/resource",
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
+      }
     ],
   },
   resolve: {
