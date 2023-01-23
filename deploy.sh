760d6cf6e7a31a32b030737581c5f8d9adbcb89b
Joera <joeramulders@gmail.com>
2023-01-23 13:34:39 +0100

    opgeschoond

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# graphs or staging
-scp public/styles/fonts/* img:/var/www/html/$1/styles/fonts/
-scp public/scripts/bundle.js img:/var/www/html/$1/scripts/bundle.js
-scp public/styles/main.css img:/var/www/html/$1/styles/main.css
-
