b9b9ea7c702bc0c4061552461500be533a78d395
Joera <joeramulders@gmail.com>
2023-01-31 12:22:59 +0100

    cleaning

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
-export const ENV = "dev"; // dev or prod
+export const ENV = "prod"; // dev or prod
 // @ts-ignore
 export const API_BASE = (ENV === "prod") ? 'https://img.publikaan.nl/open-data/api/' : 'https://img.publikaan.nl/open-data/staging/api/' ;
